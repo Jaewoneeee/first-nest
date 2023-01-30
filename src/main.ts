01cdc4ed032af4ebea9f2f8b6fb9a2d659c04e63
Jaewoneeee <chou60400@naver.com>
2023-01-30 11:09:13 +0900

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,16 @@ async function bootstrap() {
     forbidNonWhitelisted: true,// 누군가 이상한 걸 보내면, 리퀘스트 자체를 차단
     transform: true, // 예를들어 여기있는 유저들이 보낸 거를 우리가 원하는 실제 타입으로 변환해줌
   }))
+
+  const config = new DocumentBuilder()
+    .setTitle('API example')
+    .setDescription('The first API description')
+    .setVersion('1.0')
+    .addTag('jaewon')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
